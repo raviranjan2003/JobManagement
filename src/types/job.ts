9d88c8be6aff1e34747db8056c1e405b51e3c47f
Ravi <2140131@sliet.ac.in>
2025-06-16 02:27:25 +0530

    Updated job types

--- src/types/job.ts
+++ src/types/job.ts
@@ -1,5 +1,12 @@
 export interface Job {
   id: number;
+  job_title: string;
+  company_name: string;
+  location: string;
+  job_type: string;
+  salary_min: number;
+  salary_max: number;
+  application_deadline: string;
   title: string;
   logo: string;
   logoBackground: string;
