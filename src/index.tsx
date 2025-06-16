fa0616b10c58bad121136c0d5663fbd24af525bb
Ravi <2140131@sliet.ac.in>
2025-06-16 11:20:16 +0530

    removed unused comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { Assignemnt } from "./screens/Assignemnt";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    {/* <Assignemnt /> */}
   </StrictMode>,
 );
